8cabdcbf04885f1e624ecc5b611a5185e7153cfb
mvolkanaslan <mvolkanaslan@yandex.com>
2022-05-27 16:29:55 +0300

    Added Babel Configuration

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,5 @@
+// Import the css file into the index.js for adding to bundle.js
+// We use "style-loader" to transfer the properties at the style code in the bundle.js to the DOM.
+import  "../css/main.css";
 const person = require("./person");
-console.log(person.name,person.lastname + " Nasilsin ?");
+console.log(person.name,person.lastname + " Wellcome ?");
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry : './src/js/index.js',
+    entry : ["@babel/polyfill","./src/js/index.js"],//Packaging the index.js with the Polyfill package for transfer the ES6+ code to ES5
     output:{
         path: path.join(__dirname,"dist"),
         filename:'js/bundle.js'
@@ -10,5 +10,25 @@ module.exports = {
         static: {
             directory: path.join(__dirname,'dist'),
         },
+    },
+    module:{
+        rules:[
+            //this rule for only javascript packaging
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use:{
+                    loader:"babel-loader",
+                    options:{
+                        presets : ['@babel/preset-env'],
+                    }
+                }
+            },
+            //this rule for css packaging
+            {
+                test: /\.css$/i,
+                use:["style-loader","css-loader"]
+            }
+        ]
     }
 }
