d13d6cd7d8bba5ea97beb2f4f7da614befdf60bc
mvolkanaslan <mvolkanaslan@yandex.com>
2022-05-27 17:46:55 +0300

    After Packaging Bundle.js converted to ES5 version

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,8 @@
 // We use "style-loader" to transfer the properties at the style code in the bundle.js to the DOM.
 import  "../css/main.css";
 const person = require("./person");
-console.log(person.name,person.lastname + " Wellcome ?");
+
+const sayHello = async ()=>{
+    console.log(person.name,person.lastname + " Merhaba ?");
+}
+sayHello();
