18f21650f71416ccf985dc88c6a961fcab860cad
mvolkanaslan <mvolkanaslan@yandex.com>
2022-06-04 18:17:26 +0300

    bugs fixed, stability improved

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    entry : ["@babel/polyfill","./src/js/index.js"],//Packaging the index.js with the Polyfill package for transfer the ES6+ code to ES5
+    entry : ["./src/js/index.js"],
     output:{
         path: path.join(__dirname,"dist"),
         filename:'js/bundle.js',
-    },
-    devServer:{
-        static: {
-            directory: path.join(__dirname,'dist'),
-        },
     },
     module:{
         rules:[
             //this rule for only javascript packaging
             {
                 test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules/,
                 use:{
-                    loader:"babel-loader",
-                    options:{
-                        presets : ['@babel/preset-env'],
-                    }
+                    // No more settings, this will reference .babelrc
+                    loader:"babel-loader"
                 }
             },
             //this rule for css packaging
@@ -30,5 +23,12 @@ module.exports = {
                 use:["style-loader","css-loader"]
             }
         ]
-    }
+    },
+    target: "browserslist", // this will referance .broeserlistrc
+    devServer:{
+        static: {
+            directory: path.join(__dirname,'./dist'),
+        },
+    },
+
 }
